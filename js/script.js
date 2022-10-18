b5800c7ad80d66c97fd917ad193d5934b1485266
ValentinaZussino <vzussino@protonmail.com>
2022-10-18 13:07:58 +0200

    agg consegna nuova in js + agg use strict

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+// Consegna
+// Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+// L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// I numeri nella lista delle bombe non possono essere duplicati.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+// BONUS:
+// 1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+// 2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
 
-// Bonus
-// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-
-
-
+"use strict";
 // prendo btn play
 const btnPlay = document.getElementById('play');
 
@@ -36,7 +39,7 @@ btnPlay.addEventListener('click', function(){
     }
     console.log(level);
     console.log(numSquares);
-    // creo var per numero bombe e array vuoto
+    // creo var per numero bombe e array vuoto che riepmio
     const numBombs = 16;
     const bombsArray = [];
     while(bombsArray.length < numBombs){
@@ -46,7 +49,7 @@ btnPlay.addEventListener('click', function(){
         }
     }
     console.log(bombsArray);
-    // provato prima per tre ore con do while ma nada de nada de nada......
+    // provato prima per tre ore con do while (per creaz quadrati e grigli insieme) ma nada de nada de nada......
     //funzione per generare quadrato
     function createSquare(num){
             const square = document.createElement('div');
